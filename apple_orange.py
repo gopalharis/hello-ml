4d7074c97e99ad56badbbea83225deaf1afe7a3e
Gopal <gopal@socure.me>
2017-08-30 16:22:48 +0530

    Decision Tree classifier for iris and applevsorange

--- apple_orange.py
+++ apple_orange.py
@@ -1,6 +1,24 @@
 from sklearn import tree
+from sklearn.externals.six import StringIO
+import pydot
 features = [[140, 0], [130, 0], [150, 1], [170, 1]]
 labels = ["apple", "apple", "orange", "orange"]
 clf = tree.DecisionTreeClassifier()   # Empty Classifier  
-clf =  clf.fit(features, labels)    # Fit - Find Patterns in data 
+clf = clf.fit(features, labels)    # Fit - Find Patterns in data
 print clf.predict([160, 1])
+
+# Graph viz
+# conda install -c anaconda pydot=1.0.28
+# conda install GraphViz
+
+dot_data = StringIO()
+tree.export_graphviz(clf,
+                     out_file=dot_data,
+                     feature_names=["weight", "texture"],
+                     class_names=["apple", "orange"],
+                     filled=True, rounded=True,
+                     impurity=False)
+
+
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("applevsorange.pdf")
--- iris.py
+++ iris.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn import tree
 import numpy as np
+from sklearn.externals.six import StringIO
+import pydot
 
 datasets = load_iris()
 
@@ -16,3 +18,17 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(train_data, train_target)
 print clf.predict(test_data)
 
+# Graph viz
+# conda install -c anaconda pydot=1.0.28
+# conda install GraphViz
+
+dot_data = StringIO()
+tree.export_graphviz(clf,
+                     out_file=dot_data,
+                     feature_names=datasets.feature_names,
+                     class_names=datasets.target_names,
+                     filled=True, rounded=True,
+                     impurity=False)
+
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("iris.pdf")
